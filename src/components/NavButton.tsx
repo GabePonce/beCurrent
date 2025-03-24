3df794a4848c260b7635d35695ad9e4146763d1d
Gabe <gabessj4@gmail.com>
2025-03-24 14:24:36 -0300

    fixed buttons css with class name

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 const NavButton = ({children, onClick}: Props) => {
   return (
     <>
-      <button style={{fontSize: "1em", borderRadius: "1vw", border: "0.5vw solid transparent", padding: "0.75vw"}}><img src={"./src/assets/" + children} width="32vw" height="32vw"/></button>
+      <button className='navButtons' style={{fontSize: "1em", borderRadius: "1vw", border: "0.5vw solid transparent", padding: "0.75vw"}}><img src={"./src/assets/" + children} width="32vw" height="32vw"/></button>
     </>
   )
 }
