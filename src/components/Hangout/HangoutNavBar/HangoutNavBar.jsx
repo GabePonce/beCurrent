558ea22f984c996be256deae22e05a3b67a7aa56
Gabe <gabessj4@gmail.com>
2025-03-24 14:47:01 -0300

    added a comment

--- src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
+++ src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
@@ -2,7 +2,8 @@ import CustomButton from '../../Customs/CustomButton'
 
 const HangoutNavBar = () => {
   return (
-    <>
+    <> 
+        {/* sam use a placeholder image here for now i'll replace it when i pass the logic for this bit */}
         <div id="header-bar" style={{backgroundImage}}>
             
         </div>
