5d3f359383c9816d411d175bc95a26335fbced63
Cook <w0490409@campus.nscc.ca>
2025-03-25 11:46:28 -0300

    Tweaked Hangout Header Nav.

--- src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
+++ src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
@@ -6,31 +6,42 @@ const displayNavOptions = () => {
 
   var options = document.getElementById('nav-options');
 
-  window.getComputedStyle(options).display === 'none' ? options.style.display = 'block' : options.style.display = 'none';
+  if (window.getComputedStyle(options).display === 'none') {
+    options.style.display = 'block';
+  }
+  else {
+    options.style.display = 'none';
+    // document.getElementById('dropdown-arrow').innerHTML = <NavButton id="dropdown-arrow" onClick={displayNavOptions}>up.png</NavButton>; // Not working right now.
+  }
 
 }
 
 const HangoutNavBar = () => {
-  return (
+  return ( 
     <> 
         {/* The placeholder image is currently set in CSS.
             It will be a lot easier to move the CSS that handles that to an inline style
             to dynamically set it here. */}
         <div id="header-bar">
           <header id="group-image">
-            {/* You can replace the Camping with a retrieved Group name and August 17 with the retrieved date.
+            {/* You can replace Camping with a retrieved Group name and August 17 with the retrieved date.
                 Or you can not retrieve the date of the hangout and just start drinking. */}
-              <p>Camping<br/>August 17</p>
+              <p id="hangout-header-text">
+                <div id="hangout-title">Camping</div>
+                <div id="hangout-date">August 17</div>
+              </p>
           </header>
           <div id="dropdown-button-area">
-              <NavButton onClick={displayNavOptions}>down.png</NavButton>
+              <NavButton id="dropdown-arrow" onClick={displayNavOptions}>down.png</NavButton>
           </div>
         </div>
         <div id="nav-options">
-          <CustomButton>Info</CustomButton>
-          <CustomButton>List</CustomButton>
-          {/* Render only if user is the owner. */}
-          <CustomButton>Settings</CustomButton>
+          <div id="button-grid">
+            <CustomButton>Info</CustomButton>
+            <CustomButton>List</CustomButton>
+            {/* Render only if user is the owner. */}
+            <CustomButton>Settings</CustomButton>
+          </div>
         </div>
     </>
   )
