982603bac5cdd400b2ff5a4c061dbd8cf6b518df
Cook <w0490409@campus.nscc.ca>
2025-03-26 13:34:41 -0300

    Tentatively finished Hangout Nav Bar

--- src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
+++ src/components/Hangout/HangoutNavBar/HangoutNavBar.jsx
@@ -1,46 +1,53 @@
-import NavButton from '../../Navbar/NavButtons/NavButton'
 import CustomButton from '../../Customs/CustomButton';
 import './HangOutNavBar.css'
 
+
+// Handle clicking on the dropdown arrow.
 const displayNavOptions = () => {
 
+  var isOwner = false; // REPLACE THIS WITH A CHECK TO SEE IF THEY ARE THE OWNER OR NOT
+  var settingsButton = document.getElementById('settings');
+  isOwner ? settingsButton.style.display = 'block' : settingsButton.style.display = 'none';
+
   var options = document.getElementById('nav-options');
+  var dropdownButton = document.getElementById('dropdown-button');
 
   if (window.getComputedStyle(options).display === 'none') {
     options.style.display = 'block';
+    dropdownButton.innerHTML = '<img id="button-image" src="./src/assets/up.png" width="32vw" height="32vw"/>';
   }
   else {
     options.style.display = 'none';
-    // document.getElementById('dropdown-arrow').innerHTML = <NavButton id="dropdown-arrow" onClick={displayNavOptions}>up.png</NavButton>; // Not working right now.
-  }
+    dropdownButton.innerHTML = '<img id="button-image" src="./src/assets/down.png" width="32vw" height="32vw"/>';  }
 
 }
+
+// Set the name of the image for the hangout here.
+var backgroundImageName = "bubAndNans.JPG";
+
 
 const HangoutNavBar = () => {
   return ( 
     <> 
-        {/* The placeholder image is currently set in CSS.
-            It will be a lot easier to move the CSS that handles that to an inline style
-            to dynamically set it here. */}
         <div id="header-bar">
-          <header id="group-image">
+          <header id="group-image" style={{backgroundImage: 'linear-gradient(to right, transparent, #6495bd), url(./src/assets/' + backgroundImageName + ')'}}>
+
             {/* You can replace Camping with a retrieved Group name and August 17 with the retrieved date.
-                Or you can not retrieve the date of the hangout and just start drinking. */}
+                Or you can not retrieve the date of the hangout and just start day drinking. */}
               <p id="hangout-header-text">
                 <div id="hangout-title">Camping</div>
                 <div id="hangout-date">August 17</div>
               </p>
           </header>
           <div id="dropdown-button-area">
-              <NavButton id="dropdown-arrow" onClick={displayNavOptions}>down.png</NavButton>
+            <button id="dropdown-button" onClick={displayNavOptions}><img id="button-image" src={"./src/assets/down.png"} width="32vw" height="32vw"/></button>
           </div>
         </div>
         <div id="nav-options">
           <div id="button-grid">
             <CustomButton>Info</CustomButton>
             <CustomButton>List</CustomButton>
-            {/* Render only if user is the owner. */}
-            <CustomButton>Settings</CustomButton>
+            <div id="settings"><CustomButton>Settings</CustomButton></div>
           </div>
         </div>
     </>
